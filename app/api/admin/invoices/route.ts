47467ce0e7034554d106dc098cf8a1150cc706cd
margoul1Malin <github.pydoctor@gmail.com>
2025-07-19 00:30:11 +0200

    sdsd232sdsdsd3

--- app/api/admin/invoices/route.ts
+++ app/api/admin/invoices/route.ts
@@ -2,6 +2,9 @@ import { NextResponse } from 'next/server';
 import { verifyAuth } from '../../../../lib/auth';
 import prisma from '../../../../lib/prisma';
 
+// Force dynamic rendering
+export const dynamic = 'force-dynamic';
+
 export async function GET(request: Request) {
   try {
     const user = await verifyAuth();
